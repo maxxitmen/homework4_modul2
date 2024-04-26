0490c76b76d35fba4623178827594b2e9d7f418b
maxxitmen <61408791+maxxitmen@users.noreply.github.com>
2024-04-26 20:19:41 +0300

    Add files via upload

---
+++ Homework4_modul2.py
@@ -0,0 +1,6 @@
+cars_catalog = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for i in cars_catalog:
+    print('Я езжу на автомабиле марки', i)
+    cars_count = cars_count + 10
+print(cars_count)
